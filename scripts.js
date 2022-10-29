872e5feadce4f7408d937d91ed6f602ffdcb78cf
lucas-lima-dev <lucas.sdslima@gmail.com>
2022-10-29 16:09:57 -0300

    keep conection ready

--- scripts.js
+++ scripts.js
@@ -1,81 +1,100 @@
 let conversas = [];
+let nameUser = '';
 
-const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-
-// console.log('Enviou o pedido');
-promise.then(answerArrived);
-//promise.catch(serverErro);
+getMessages();
 
 askNameUser();
 
-function answerArrived(answer) {
-    console.log("RESPOSTA CHEGOU");
-    console.log(answer);
-    console.log(answer.data);
+setInterval(keepConection,5000);
 
-    conversas = answer.data;
+function getMessages() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then((res) => {renderizarConversas(res.data)});
+    promise.catch(() => console.log("Mensagem de erro: Erro de requisicao do servidor"));
 }
+
 
 function askNameUser(){
 
-   // let nameUser = prompt('Qual o seu nome?');
-   let nameUser = 'ana';
+   // nameUser = prompt('Qual o seu nome?');
+   nameUser = 'daksdmqinqsncajscasjx[q';
 
     const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',{name:nameUser});
 
-   promise.then(tratarNome);
-
-    const chatList = document.querySelector('.chat') ;
-    chatList.innerHTML += `<li class = 'joinedLeaveRoom'><span class = 'time'>(09:21:45)</span> <spam class = 'name'>${nameUser}</spam> entra na sala...</li>`;
-
-    promise.catch(deuErro);
-
-}
-
-
-
-function tratarNome(element) {
-
-    console.log("RESPOSTA CHEGOU");
-    console.log(element);
-    console.log(element.status); 
-
-    if(element.status !== 200 ){
-        alert('Já existe um usuario online com esse nome digitado, por favor digite outro nome:');
+    promise.then();
+    promise.catch(() => {
+        alert('Já existe um usuario online com esse nome digitado, por favor digite outro nome.');
         askNameUser();
-    } else return 
-        
+    });
+    
 }
 
-function deuErro(erro) {
-    alert("Mensagem de erro: " + erro.data);
+function keepConection() {
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/status',{name:nameUser});
+    promise.then()
+    promise.catch(() => console.log("Mensagem de erro: Erro de requisicao do servidor"));
 }
 
 
 
 function addMessage(){
+
     const typedText = document.querySelector('input').value;
-    //const newMessages = {time:,name:,talkTo:,message:typedText};
+
+    const newMessages = `{
+        from: ${nameUser},
+        to: "nome do destinatário (Todos se não for um específico)",
+        text: ${typedText},
+        type: "message" 
+    };`;
+
     //conversas.push(newMessages);
 
-    const promessa = axios.post('',newMessages);
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/messages',newMessages);
     // promessa.then();
-
-
-
-
-    renderizarConversas()
+    //promessa.catch((erro) => alert("Mensagem de erro: " + erro.data));
+    //renderizarConversas(newMessages);
 }
 
-function renderizarConversas(){
-    //const chatList = document.querySelector('.chat') ;
+function renderizarConversas(message){
 
-    //chatList.innerHTLM = '';
+    const chatList = document.querySelector('.chat');
+    const filteredMessage = filterMessages(message);
+    console.log(filteredMessage);
 
-//     for(let i =0; i < conversas.length; i++){
+    chatList.innerHTLM = '';
 
-//         let conversa = conversas[i];
+    for(let i =0; i < filteredMessage.length; i++){
 
-//        // chatList.innerHTML += ``;
-      //}
+        const objMessage = filteredMessage[i];
+
+        if(filteredMessage[i].type === 'status') {
+            chatList.innerHTML += `<li class = 'joinedLeaveRoom'><span class = 'time'>${objMessage.time}</span> <spam class = 'name'>${objMessage.from}</spam> ${objMessage.text}</li>`;
+        } else if (filteredMessage[i].type === 'message') {
+            chatList.innerHTML += `<li><span class = 'time'>${objMessage.time}</span> <spam class = 'name'>${objMessage.from}</spam> para <spam class = 'name'>Todos</spam>: ${objMessage.text}</li>`;
+        } else {
+            chatList.innerHTML += `<li class = 'privateMessage'><span class = 'time'>${objMessage.time}</span> <spam class = 'name'>${objMessage.from}</spam> reservadamente para <spam class = 'name'>${nameUser}</spam>: ${objMessage.text}</li>`;
+        }
+    
+    }
+
+      
+}
+
+function filterMessages(message) {
+
+    const filteredMessage = [];
+
+    for(let i =0; i < message.length; i++){
+
+        if(message[i].type !== 'private_message') {
+            filteredMessage.push(message[i]);
+        } else if (message[i].type === 'private_message' && message[i].to === nameUser){
+            filteredMessage.push(message[i]);
+        }
+
+    }
+
+    return filteredMessage;
+
 }
